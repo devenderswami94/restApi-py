a0a97eb61388614b579077b30b362ed839555980
devenderswami94 <devenderswami94@gmail.com>
2020-04-09 18:25:18 +0530

    data for the api

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'profiles_api',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
