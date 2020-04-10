98332dc9da880391ff5dafa64c728c275727a3d2
devenderswami94 <devenderswami94@gmail.com>
2020-04-10 17:26:45 +0530

    data for the api jj ss

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     serializer_class = serializers.HelloSerializer
@@ -39,3 +44,51 @@ class HelloApiView(APIView):
 
     def delete(self,request,pk=None):
         return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    def list(self,request):
+
+        a_viewset = [
+            'actions',
+            'automatically map',
+            'provides functionality'
+        ]
+
+        return Response({'message':'Hello!','a_viewset':a_viewset})
+
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name= serializer.validated_data.get('name')
+            message= f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def retrieve(self,request,pk=None):
+        return Response({'http_method':'GET'})
+
+    def update(self,request,pk=None):
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        return Response({'http_method':'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
